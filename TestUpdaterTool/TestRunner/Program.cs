59348ebea43f24b432fc5cfa9460116adf7f9b92
taja01 <jarvas.tamas@gmail.com>
2025-04-22 23:58:06 +0200

    add runner

---
+++ TestUpdaterTool/TestRunner/Program.cs
@@ -0,0 +1,41 @@
+﻿using Common;
+using TestCaseUpdater;
+
+namespace TestRunner
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            // Configuration Parameters
+            string organization = "TamasJarvas";          // Azure DevOps Organization
+            string project = "Sandbox";                    // Azure DevOps Project
+            string personalAccessToken = ""; // PAT Token
+
+            // Initialize Azure DevOps Service
+            AzureDevOpsService adoService = new(organization, project, personalAccessToken);
+
+            // Define Test Steps
+            var testSteps = new List<TestStep> {  new()
+                {
+                    Action = "Given I am in the main page",
+                    Expected = "Main page loaded"
+                },
+                new()
+                {
+                    Action = "When I click login button",
+                    Expected = "Login popup appears"
+                }
+            };
+
+            // Test Case ID
+            int testCaseId = 14;
+
+            // Update test case steps
+            await adoService.UpdateTestCaseStepsAsync(testCaseId, testSteps, "My Test Title 2");
+
+            // Dispose resources
+            adoService.Dispose();
+        }
+    }
+}
