ba710812e744c7b632bf1403a58f450af2901ac4
taja01 <jarvas.tamas@gmail.com>
2026-02-14 09:23:08 +0100

    move registration to extension method

---
+++ TestUpdaterTool/TestCaseManager/Services/AzureDevOpsServiceExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Polly;
+using System.Text;
+using TestCaseManager.Configurations;
+using TestCaseManager.Contracts;
+
+namespace TestCaseManager.Services
+{
+    public static class AzureDevOpsServiceExtension
+    {
+        public static IServiceCollection AddAzureDevOpsServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Configure AzureDevOps options with validation
+            services.AddOptions<AzureOptions>()
+                .Bind(configuration.GetSection(nameof(AzureOptions)))
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            // Register HttpClient with policies
+            services.AddHttpClient<ITestUpdateService, AzureDevOpsService>((serviceProvider, client) =>
+            {
+                var config = serviceProvider.GetRequiredService<IOptions<AzureOptions>>().Value;
+
+                client.BaseAddress = new Uri($"https://dev.azure.com/{config.Organization}/{config.Project}/_apis/");
+
+                var authToken = Convert.ToBase64String(Encoding.ASCII.GetBytes($":{config.PersonalAccessToken}"));
+                client.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authToken);
+            })
+            .AddTransientHttpErrorPolicy(policy =>
+                policy.WaitAndRetryAsync(3, retryAttempt =>
+                    TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))))
+            .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(30)));
+
+            return services;
+        }
+    }
+}
--- TestUpdaterTool/TestRunner/Program.cs
+++ TestUpdaterTool/TestRunner/Program.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using Polly;
 using Serilog;
-using System.Text;
-using TestCaseManager.Configurations;
-using TestCaseManager.Contracts;
 using TestCaseManager.Services;
 using TestParser.Contracts;
 using TestParser.Parsers;
@@ -47,33 +42,14 @@ namespace TestRunner
                     })
                     .ConfigureServices((context, services) =>
                     {
-                        // Configure AzureDevOps options with validation
-                        services.AddOptions<AzureOptions>()
-                            .Bind(context.Configuration.GetSection("AzureOptions"))
-                            .ValidateDataAnnotations()
-                            .ValidateOnStart();
-
                         // Configure TestRunner options
                         services.AddOptions<TestRunnerOptions>()
                             .Bind(context.Configuration.GetSection("TestRunnerOptions"))
                             .ValidateDataAnnotations()
                             .ValidateOnStart();
 
-                        // Register HttpClient-based service - lifetime managed by AddHttpClient
-                        services.AddHttpClient<ITestUpdateService, AzureDevOpsService>((serviceProvider, client) =>
-                        {
-                            var config = serviceProvider.GetRequiredService<IOptions<AzureOptions>>().Value;
-
-                            client.BaseAddress = new Uri($"https://dev.azure.com/{config.Organization}/{config.Project}/_apis/");
-
-                            var authToken = Convert.ToBase64String(Encoding.ASCII.GetBytes($":{config.PersonalAccessToken}"));
-                            client.DefaultRequestHeaders.Authorization =
-                                new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authToken);
-                        })
-                        .AddTransientHttpErrorPolicy(policy =>
-                            policy.WaitAndRetryAsync(3, retryAttempt =>
-                                TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))))
-                        .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(30)));
+                        // Add Azure DevOps services using extension method
+                        services.AddAzureDevOpsServices(context.Configuration);
 
                         // Register application services
                         services.AddTransient<IFileHandler, FileHandler>();
